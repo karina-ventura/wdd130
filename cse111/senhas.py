d63096f94aef315013f44f4ab9ec94a829527392
karina-ventura <kventuradeoliveira@byupathway.edu>
2026-05-14 19:48:34 -0300

    Corrigindo página inicial

---
+++ cse111/senhas.py
@@ -0,0 +1,28 @@
+MINUSCULAS=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+             "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+MAIUSCULAS=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+             "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+DIGITOS=["0","1","2","3","4","5","6","7","8","9"]
+
+ESPECIAIS=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+",
+           "[", "]", "{", "}", "|", ";", ":", """, """, ",", ".", "<", ">", "?", "/", "`", "~"]
+
+def procurar_palavra(palavra, nome_do_arquivo, maiusculas_e_minusculas=False):
+
+    with open(nome_do_arquivo, "r", encoding="utf-8"):
+
+        
+        
+def palavra_tem_caractere(palavra, lista_caracteres):
+   pass
+
+def calcular_complexidade(palavra):
+   pass
+
+def validar_senha(senha, comprimento_min=10, comprimento_forte=16):
+   pass
+
+def main():
+   pass
