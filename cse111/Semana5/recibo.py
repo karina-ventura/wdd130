f2acb6d650393cdf7f202ccbc25636381a2d8e57
karina-ventura <kventuradeoliveira@byupathway.edu>
2026-06-06 14:27:54 -0300

    Atividade página de contato rafting

--- cse111/Semana5/recibo.py
+++ cse111/Semana5/recibo.py
@@ -1,4 +1,8 @@
+# Foi adicionado ao programa a exibição da data limite para devolução.
+# A data é definida automaticamente para 30 dias após a compra, às 21h.
+
 import csv
+from datetime import datetime, timedelta
 
 def ler_dicionario(filename, indice_coluna_chave):
     dicionario = {}
@@ -15,29 +19,71 @@ def ler_dicionario(filename, indice_coluna_chave):
     return dicionario
 
 def main():
-    dic_produtos = ler_dicionario("produtos.csv", 0)
 
-    print("Todos os produtos")
-    print(dic_produtos)
+    try:
 
-    print()
-    print("Itens pedidos")
+        dic_produtos = ler_dicionario("produtos.csv", 0)
 
-    with open("pedido.csv", "rt", encoding="utf-8") as arquivo_pedido:
-        leitor = csv.reader(arquivo_pedido)
+        print("Ponto Certo Mercearia")
+        print()
 
-        next(leitor)
+        numero_itens = 0
+        subtotal = 0
 
-        for linha in leitor:
-            id_produto = linha[0]
-            quantidade = linha[1]
+        print()
+        print("Itens pedidos")
 
-            produto = dic_produtos[id_produto]
+        with open("pedido.csv", "rt", encoding="utf-8") as arquivo_pedido:
+            leitor = csv.reader(arquivo_pedido)
 
-            nome = produto[1]
-            preco = produto[2]
+            next(leitor)
 
-            print(f"{nome}: {quantidade} @ {preco}")
+            for linha in leitor:
+                id_produto = linha[0]
+                quantidade = int(linha[1])
+
+                produto = dic_produtos[id_produto]
+
+                nome = produto[1]
+                preco = float(produto[2])
+
+                print(f"{nome}: {quantidade} @ {preco:.2f}")
+
+                numero_itens += quantidade
+                subtotal += quantidade * preco
+
+        imposto = subtotal * 0.06
+        total = subtotal + imposto
+
+        print()
+        print(f"Número de itens: {numero_itens}")
+        print(f"Subtotal: {subtotal:.2f}")
+        print(f"Imposto sobre vendas: {imposto:.2f}")
+        print(f"Total: {total:.2f}")
+
+        print()
+        print("Obrigado por comprar no Ponto Certo Mercearia.")
+
+        agora = datetime.now()
+        print(agora.strftime("%d/%m/%Y %H:%M:%S"))
+
+        print()
+        print("Data limite para devolução: ")
+        data_devolucao = agora + timedelta(days=30)
+        data_devolucao = data_devolucao.replace(hour=21, minute=0, second=0)
+        print(data_devolucao.strftime("%d/%m/%Y %H:%M:%S"))
+
+    except FileNotFoundError as erro:
+        print("Error: missing file")
+        print(erro)
+
+    except PermissionError as erro:
+        print("Error: permission denied")
+        print(erro)
+
+    except KeyError as erro:
+        print("Error: unknown product ID in the request.csv file")
+        print(erro)
 
 
 if __name__ == "__main__":
